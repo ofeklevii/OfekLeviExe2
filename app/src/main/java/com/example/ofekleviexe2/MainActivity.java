228f4d68f6f55c319c50ef49c8684d6159437402
ofeklevii <ofeklevi@sisma.org.il>
2024-01-08 21:36:08 +0200

    finish

--- app/src/main/java/com/example/ofekleviexe2/MainActivity.java
+++ app/src/main/java/com/example/ofekleviexe2/MainActivity.java
@@ -7,12 +7,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn;
     int sum = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
     }
 
     public void exe2(View view) {
